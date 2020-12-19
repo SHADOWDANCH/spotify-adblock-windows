e1f73de240bcb87baaf12c392cbc0eacfacaa294
SHADOWDAN <chdanilpro@gmail.com>
2020-12-19 21:03:19 +0200

    Update or something idk

--- hooks/hooks.cpp
+++ hooks/hooks.cpp
@@ -1,101 +1,108 @@
 #include "stdafx.h"
 #include <easyhook.h>
 #include <fstream>
-#include <string>
-#include <list>
-#include <stdio.h>
-#include <time.h>
 #include <WS2tcpip.h>
+#include <include/capi/cef_urlrequest_capi.h>
+#include <shlwapi.h>
 
-std::list<std::string> whitelist = {};
+#include "blacklist.h"
+#include "whitelist.h"
 
-void Log(const std::string& severity, const std::string& text)
-{
-	// Get date
-	char timeBuff[20];
-	struct tm *sTm;
-	time_t now = time(0);
-	sTm = localtime(&now);
-	strftime(timeBuff, sizeof(timeBuff), "%Y-%m-%d %H:%M:%S", sTm);
+const size_t whitelist_size = sizeof(whitelist) / sizeof(whitelist[0]);
+const size_t blacklist_size = sizeof(blacklist) / sizeof(blacklist[0]);
 
+#if _DEBUG
+void printlog(const std::string& text) {
 	// Write to file
 	std::ofstream logfile("adblock_log.txt", std::ios_base::out | std::ios_base::app);
-	logfile << "[" << timeBuff << "][" << severity << "] " << text << std::endl;
+	logfile << text << std::endl;
+}
+#endif
+
+bool listed(const char* item, const char* list[], size_t list_size) {
+	for (size_t i = 0; i < list_size; i++) {
+		if (PathMatchSpecExA(item, list[i], PMSF_NORMAL) == S_OK) {
+			return true;
+		}
+	}
+	return false;
 }
 
-INT WSAAPI getaddrinfoHook(const char* pNodeName, const char* pServiceName, const ADDRINFOA* pHints, PADDRINFOA* ppResult)
-{
-	std::string hostname(pNodeName);
-	bool whitelisted = false;
-
-	// Iterate through whitelist
-	for (auto const& i : whitelist)
-	{
-		if (i == hostname) // check if hostname exactly matches
-		{
-			whitelisted = true;
-		}
-		else if (i.find('*') != std::string::npos) // very basic wildcard support
-		{
-			// Split string by *
-			std::string token = i.substr(i.find('*') + 1); // right of *
-
-			// Check if hostname includes token
-			if (hostname.find(token) != std::string::npos)
-			{
-				whitelisted = true;
-			}
-		}
+INT WSAAPI getaddrinfoHook(const char* pNodeName, const char* pServiceName, const ADDRINFOA* pHints, PADDRINFOA* ppResult) {
+	if (listed(pNodeName, whitelist, whitelist_size)) {
+#if _DEBUG
+		printlog(std::string("[+] getaddrinfo:\t\t").append(pNodeName));
+#endif
+		return getaddrinfo(pNodeName, pServiceName, pHints, ppResult);
 	}
+#if _DEBUG
+	printlog(std::string("[-] getaddrinfo:\t\t").append(pNodeName));
+#endif
+	return EAI_FAIL;
+}
 
-	if (!whitelisted)
-	{
-		Log("INFO", "Blocking " + hostname);
-		pNodeName = "0.0.0.0";
+cef_urlrequest_t* cef_urlrequest_createHook(_cef_request_t* request, struct _cef_urlrequest_client_t* client, struct _cef_request_context_t* request_context) {
+	cef_string_userfree_utf16_t url_utf16 = request->get_url(request);
+	char* url = new char[url_utf16->length + 1];
+	url[url_utf16->length] = '\0';
+	for (int i = 0; i < url_utf16->length; i++) url[i] = *(url_utf16->str + i);
+	cef_string_userfree_utf16_free(url_utf16);
+	if (listed(url, blacklist, blacklist_size)) {
+#if _DEBUG
+		printlog(std::string("[-] cef_urlrequest_create:\t").append(url));
+#endif
+		return NULL;
 	}
-	else
-	{
-		Log("INFO", "Allowing " + hostname);
-	}
-
-	return getaddrinfo(pNodeName, pServiceName, pHints, ppResult);
+#if _DEBUG
+	printlog(std::string("[+] cef_urlrequest_create:\t").append(url));
+#endif
+	return cef_urlrequest_create(request, client, request_context);
 }
 
 extern "C" void __declspec(dllexport) __stdcall NativeInjectionEntryPoint(REMOTE_ENTRY_INFO* inRemoteInfo);
-void __stdcall NativeInjectionEntryPoint(REMOTE_ENTRY_INFO* inRemoteInfo)
-{
+void __stdcall NativeInjectionEntryPoint(REMOTE_ENTRY_INFO* inRemoteInfo) {
+#if _DEBUG
 	// Init logging
 	std::remove("adblock_log.txt");
-	Log("INFO", "NativeInjectionEntryPoint called");
+	printlog("NativeInjectionEntryPoint called");
+#endif
 
-	// Read whitelist file
-	std::ifstream infile("whitelist.txt");
-	for (std::string line; std::getline(infile, line);)
-	{
-		whitelist.push_back(line);
-	}
-	Log("INFO", "Read " + std::to_string(whitelist.size()) + " entries from whitelist");
-
-	// Perform hooking
-	HOOK_TRACE_INFO hHook = { NULL };
-
-	// Install the hook
-	NTSTATUS result = LhInstallHook(GetProcAddress(GetModuleHandle(TEXT("ws2_32")), "getaddrinfo"), getaddrinfoHook, NULL, &hHook);
-	if (FAILED(result))
-	{
-		std::wstring err(RtlGetLastErrorString());
-		Log("ERROR", "Failed to install hook: " + std::string(err.begin(), err.end()));
-	}
-	else
-	{
-		Log("INFO", "Hook installed successfully");
-	}
+	HMODULE ws2_32 = GetModuleHandle(TEXT("ws2_32"));
+	HMODULE libcef = GetModuleHandle(TEXT("libcef"));
 
 	// If the threadId in the ACL is set to 0, then internally EasyHook uses GetCurrentThreadId()
 	ULONG ACLEntries[1] = { 0 };
 
-	// Disable the hook for the provided threadIds, enable for all others
-	LhSetExclusiveACL(ACLEntries, 1, &hHook);
+	bool fail = false;
+
+	// Install the hook
+	if (ws2_32 != NULL) {
+		HOOK_TRACE_INFO hGetAddrInfoHook = { NULL };
+		NTSTATUS resultGetAddrInfo = LhInstallHook(GetProcAddress(ws2_32, "getaddrinfo"), getaddrinfoHook, NULL, &hGetAddrInfoHook);
+		// Disable the hook for the provided threadIds, enable for all others
+		LhSetExclusiveACL(ACLEntries, 1, &hGetAddrInfoHook);
+
+		fail = FAILED(resultGetAddrInfo);
+	}
+
+	// Install the hook
+	if (libcef != NULL) {
+		HOOK_TRACE_INFO hUrlRequestHook = { NULL };
+		NTSTATUS resultUrlrequest = LhInstallHook(GetProcAddress(libcef, "cef_urlrequest_create"), cef_urlrequest_createHook, NULL, &hUrlRequestHook);
+		// Disable the hook for the provided threadIds, enable for all others
+		LhSetExclusiveACL(ACLEntries, 1, &hUrlRequestHook);
+
+		fail = FAILED(resultUrlrequest);
+	}
+
+#if _DEBUG
+	if (fail){
+		std::wstring err(RtlGetLastErrorString());
+		printlog("Failed to install hook: " + std::string(err.begin(), err.end()));
+	} else {
+		printlog("Hook installed successfully");
+	}
+#endif
 
 	return;
 }
